ecaa4c518bf62a8cf6f85c1fec66102a9ce9622f
Joan <joangoma24@gmail.com>
2023-05-22 22:06:12 +0200

    billboard func added

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from rich import emoji
 from rich.console import Console
+from rich.markdown import Markdown
 from rich.prompt import Prompt
 from rich.table import Table
 
@@ -11,28 +12,33 @@ console = Console()
 # Function to draw the menu
 def draw_menu():
     console.clear()
-    console.print("CINE BUS :bus: 🎥", style="bold")
-    console.print("Autors: Laia Mogas i Joan Gomà 😎", style="bold")
-    console.print("\nOpcions:\n")
+    MARKDOWN = """
+# CINE BUS 🚌 🎥
+## Autors: Laia Mogas i Joan Gomà 
+Opcions:
 
-    console.print(":one:  Mostra la cartellera")
-    console.print(":two:  Cercar en la cartellera")
-    console.print(":three:  Mostra el graf de busos")
-    console.print(":four:  Mostra el graf de la ciutat")
-    console.print(":five:  Tria cine i ubicació")
+1. Mostra la cartellera
+2. Cerca en la cartellera
+3. Mostra el graf de busos
+4. Mostra el graf de la ciutat
+5. Tria peli i ubicació
+"""
 
+    md = Markdown(MARKDOWN)
+    console.print(md)
     console.print()
 
 
-def draw_billboard(billboard: Billboard) -> None:
+def show_billboard(billboard: Billboard) -> None:
+    """Shows the billboard info in a table format."""
+
     table = Table(show_header=True, header_style="bold magenta")
     table.add_column("Cinema", justify="left")
     table.add_column("Pel·lícula")
     table.add_column("Durada")
     table.add_column("Horari")
 
-    key = Prompt.ask("Filtra per: ", choices=["time", "genre", "duration"])
-
+    time = 18
     for i, projection in enumerate(sorted(billboard.projections, key=lambda x: x.time)):
         table.add_row(
             projection.cinema.name,
@@ -47,13 +53,64 @@ def draw_billboard(billboard: Billboard) -> None:
     console.print(table)
 
 
-# Function to handle user input
-def handle_input(key: str, billboard: Billboard):
+def show_projections(projections: list[Projection]) -> None:
+    """Show a set of films to the user"""
+
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("Cinema", justify="left")
+    table.add_column("Pel·lícula")
+    table.add_column("Durada")
+    table.add_column("Horari")
+
+    time = 18
+    for i, projection in enumerate(sorted(projections, key=lambda x: x.time)):
+        table.add_row(
+            projection.cinema.name,
+            projection.film.title,
+            str(projection.duration),
+            "{:<02d}".format(projection.time[0])
+            + ":"
+            + "{:<02d}".format(projection.time[1]),
+        )
+
+    console.print("\nCartellera en horari no decreixent:\n")
+    console.print(table)
+
+
+def search_billboard(billboard: Billboard) -> None:
+    """Show the films that fulfill the given constraints by the user"""
+
+    key = Prompt.ask(
+        "Filtra la busca de la pel·lícula per:",
+        choices=["title", "starting time", "duration"],
+    )
+
+    projections: list[Projection] = list()
+    if key == "title":
+        word = Prompt.ask("Introdueixi el títol de la seva pel·lícula")
+        projections = billboard.search_projection_by_word(word)
+    if key == "starting time":
+        start_time = Prompt.ask(
+            "Introdueixi a partir de quina hora vol anar a veure la pel·lícula (ex: 12:30))"
+        )
+        hour, minute = start_time.split(":")
+        projections = billboard.search_projection_by_time((int(hour), int(minute)))
+
+    if key == "duration":
+        durada = int(
+            Prompt.ask("Introdueixi la durada que vol que duri la seva pel·lícula")
+        )
+        projections = billboard.search_projection_by_duration(durada)
+
+    show_projections(projections)
+
+
+def handle_input(key: str, billboard: Billboard) -> None:
+    """Function that handles user input."""
     if key == "1":
-        console.clear()
-        draw_billboard(billboard)
+        show_billboard(billboard)
     elif key == "2":
-        ...
+        search_billboard(billboard)
     elif key == "3":
         ...
     elif key == "4":
@@ -65,7 +122,7 @@ def handle_input(key: str, billboard: Billboard):
 
 
 def main() -> None:
-    billboard: Billboard = read()
+    billboard: Billboard = read_billboard()
     while True:
         draw_menu()
         key = Prompt.ask("Selecciona el nombre de la acció que vulgui dur a terme")
