7a9dc19f597eaace0c743571689d9500f1b54e56
lmp315 <laiamogasp@gmail.com>
2023-06-01 15:04:57 +0200

    some comments added

--- city2.py
+++ city2.py
@@ -25,6 +25,13 @@ BUS_WAIT_TIME = 8.0 #minutes
 FILE_OSMNX_NAME = "OSMNX_GRAPH"
 FILE_CITY_NAME = "CITY_GRAPH"
 
+'''
+OSM -> 41, 2 latitud y longitud
+STATICMAP -> 2, 41 (LON- LAT)
+BUSES -> 41, 2 UTM
+ox.distance.nearest_nodes -> 2, 41 LON - LAT
+'''
+
 
 def get_only_first_edge_and_del_geometry(g: OsmnxGraph) -> OsmnxGraph:
     # for each node and its neighbours' information ...
@@ -40,15 +47,9 @@ def get_only_first_edge_and_del_geometry(g: OsmnxGraph) -> OsmnxGraph:
                 del(e_attr['geometry'])
             #print('        ', e_attr)
 
-'''
-OSM -> 41, 2 latitud y longitud
-STATICMAP -> 2, 41 (LON- LAT)
-BUSES -> 41, 2 UTM
-ox.distance.nearest_nodes -> 2, 41 LON - LAT
-'''
 
 def get_osmnx_graph() -> OsmnxGraph:
-    '''Returns a graph of the streets of Barcelona. Get_only_first_edge should be debugged'''
+    '''Returns a graph of the streets of Barcelona. Get_only_first_edge should be tested'''
     path = os.getcwd() + '\\' + FILE_OSMNX_NAME
     if os.path.exists(path):
         return load_graph(FILE_OSMNX_NAME)
@@ -61,8 +62,8 @@ def get_osmnx_graph() -> OsmnxGraph:
         return g
 
 
-def save_graph(g: OsmnxGraph, filename: str) -> None:
-    '''Saves multigraph g in file {filename}'''
+def save_graph(g: OsmnxGraph | CityGraph, filename: str) -> None:
+    '''Saves graph g in file {filename}'''
 
     pickle_out = open(filename, "wb")
     pickle.dump(g, pickle_out)
@@ -70,7 +71,7 @@ def save_graph(g: OsmnxGraph, filename: str) -> None:
 
 
 def load_graph(filename: str) -> OsmnxGraph | CityGraph:
-    '''Returns the multigraph stored in file filename'''
+    '''Returns the graph stored in file filename'''
 
     pickle_in = open(filename, "rb")
     return pickle.load(pickle_in)
@@ -88,23 +89,25 @@ def join_parada_cruilla(city, buses, cruilles) -> None:
 
 
 def get_weight(a, b, attr):
+    '''Used to estimate the time taken to go from one bus stop to another.'''
     return attr['weight'] * WALK_SPEED / BUS_SPEED
 
 
 def add_weights(city: CityGraph) -> None:
-    '''
+    '''The attribute weight of each edge is set.
 
     parada1-liniaA i parada1-liniaB tenen weight = delay
     parada1-liniaA i parada2-liniaA tenen weight = distancia
 
-
     NO hi ha una aresta entre parada1-liniaA i parada2-liniaB
+
+    Primer es calculen els pesos dels carrers ja que es necessiten per calcular els pesos dels busos
     '''
 
     '''Per calcular la distancia entre parades necessito distancia entre cruilles'''
     for edge in city.edges(data = True):
         if edge[2]['type'] == 'Carrer':
-            #haversine retorna distancia en km
+            #haversine returns the distance in km
             city.edges[edge[0], edge[1]]['weight'] = haversine(city.nodes[edge[0]]['coord'], city.nodes[edge[1]]['coord'])/WALK_SPEED*60
 
 
@@ -118,7 +121,7 @@ def add_weights(city: CityGraph) -> None:
             city.edges[edge[0], edge[1]]['weight'] = nx.shortest_path_length(city, source = cruilla1, target = cruilla2, weight= get_weight)
 
     #afegeixo arestes entre subparades duna mateixa parada amb weight = delay
-    parades = {} #agrupo les subparades per parades
+    parades: dict[str, list[str]] = {} #agrupo les subparades per parades
     for node in city.nodes(data = True):
         if node[1]['type'] == "Parada":
             parada = node[0].split("-")[0]
@@ -132,12 +135,18 @@ def add_weights(city: CityGraph) -> None:
 
 
 def build_city_graph(g1: OsmnxGraph, g2: BusesGraph) -> CityGraph:
-    '''Merges g1 and g2 to build a Citygraph.
+    '''If the citygraph is stored in FILE_CITY_NAME, it is loaded. Otherwise, g1 and g2 are merged to build a Citygraph
+    and it is saved in FILE_CITY_NAME.
 
     Types of nodes and edges are added (nodes: Cruilla/Parada, edges: Carrer/Bus).
     Coordinates of osmnxgraph nodes are swapped. We also ignore street count. {'y': 41.4259553, 'x': 2.1866781, 'street_count': 3}) -> format anterior
-
     '''
+
+    path = os.getcwd() + '\\' + FILE_CITY_NAME
+    if os.path.exists(path):
+        return load_graph(FILE_CITY_NAME)
+
+
     city: CityGraph = CityGraph() #ns si es pot escriure així
 
     #nodes g1:
@@ -147,37 +156,38 @@ def build_city_graph(g1: OsmnxGraph, g2: BusesGraph) -> CityGraph:
     #nodes g2:
     city.add_nodes_from(g2.nodes(data = True), type = "Parada")
 
-    #arestes g1:
+    #edges g1:
     for edge in g1.edges(data = True):
         if edge[0] != edge[1]: #hi havia loops a city
             city.add_edge(edge[0], edge[1], name = edge[2].get('name', None), type = "Carrer", weight = float('inf'))
 
-    #arestes g2:
+    #edges g2:
     city.add_edges_from(g2.edges(data = True), type = "Bus", weight = float('inf')) #conservem totes les dades
 
     join_parada_cruilla(city, g2, g1)
 
+    #PODRIEM AFEGIR EL WEIGHT DELS CARRERS QUAN AFEGIM ELS EDGES I Q LA FUNCIO ES DIGUI ADD_WEIGHTS_BUSES
     add_weights(city) #com mes valors poguem tenir precalculats millor
+
+    save_graph(city, FILE_CITY_NAME)
 
     return city
 
 
 def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path:
-    '''nota: ox.distance.nearest_nodes necessita coordenades girades
-        shortest_path: If the source and target are both specified, return a single list of nodes in a shortest path from the source to the target.
+    '''Returns a tuple with the list of nodes ids of the shortest path from src to dst and the minutes it takes.'''
+    '''nota: shortest_path: If the source and target are both specified, return a single list of nodes in a shortest path from the source to the target.
     '''
-
+    #  ox.distance.nearest_nodes uses lon-lat coordinates, so we need to swap them
     cruilla_src, cruilla_dst = ox.distance.nearest_nodes(ox_g, [src[1], dst[1]], [src[0], dst[0]], return_dist=False)
 
-    #tots els edges tenen pes
-    nodes_path: list[T] = nx.shortest_path(g, source = cruilla_src, target = cruilla_dst, weight = 'weight')
+    nodes_path: list[str] = nx.shortest_path(g, source = cruilla_src, target = cruilla_dst, weight = 'weight')
 
     return (nodes_path, nx.path_weight(g, nodes_path, 'weight'))
 
 
 def show(g: CityGraph) -> None:
-    '''mostra g de forma interactiva en una finestra
-    shows the graph interactively using network.draw'''
+    '''Shows the graph g interactively using network.draw'''
 
     positions = nx.get_node_attributes(g, "coord")
     nx.draw(g, pos=positions, with_labels = False, node_size=10)
@@ -185,7 +195,7 @@ def show(g: CityGraph) -> None:
 
 
 def plot(g: CityGraph, filename: str) -> None:
-    '''desa g com una imatge amb el mapa de la ciutat de fons en l'arxiu filename'''
+    '''Saves the graph g as an image with the city map in the background in the file filename'''
 
     map = StaticMap(300, 300)
 
@@ -230,30 +240,29 @@ def plot_path(g: CityGraph, p: Path, filename: str, *args) -> None:
 
     colors_linies: dict[str, tuple[int, int, int]] = get_colors_from_path(g, p)
 
-
     # draw nodes
-    for i, node in enumerate(p[0]):
-
-        if g.nodes[node]['type'] == "Parada":
+    for i, id_node in enumerate(p[0]):
+        node = g.nodes[id_node] #dict with attributes
+        if node['type'] == "Parada":
             color = "red"
 
-        elif g.nodes[node]['type'] == "Cruilla":
+        elif node['type'] == "Cruilla":
             color = "blue"
 
-        map.add_marker(CircleMarker((g.nodes[node]['coord'][1], g.nodes[node]['coord'][0]), color, 3))
+        map.add_marker(CircleMarker((node['coord'][1], node['coord'][0]), color, 3))
 
         if i != 0: #draw edges
-            prev_node = p[0][i-1]
+            prev_node = g.nodes[p[0][i-1]]
 
-            coord_1 = (g.nodes[node]['coord'][1], g.nodes[node]['coord'][0])
-            coord_2 = (g.nodes[prev_node]['coord'][1], g.nodes[prev_node]['coord'][0])
+            coord_1 = (node['coord'][1], node['coord'][0])
+            coord_2 = (prev_node['coord'][1], prev_node['coord'][0])
 
 
-            #si un dels dos es cruilla s'ha d'anar a peu
-            if g.nodes[node]['type'] == "Cruilla" or g.nodes[prev_node]['type'] == "Cruilla":
+            #si un dels dos es cruilla s'ha d'anar a peu -> color blau
+            if node['type'] == "Cruilla" or prev_node['type'] == "Cruilla":
                 color = "blue"
             else:
-                color = colors_linies[g.nodes[node]['linia']]
+                color = colors_linies[node['linia']]
 
             map.add_line(Line([coord_1, coord_2], color, 2))
 
